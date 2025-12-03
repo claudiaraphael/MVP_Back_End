70cb7c356e4c4c024553de30e2be64963bedf58f
main <claudia.raphael73@gmail.com>
2025-12-03 14:48:23 -0300

    update comment.py and product.py to import the global db

--- model/comment.py
+++ model/comment.py
@@ -1,8 +1,6 @@
-from sqlalchemy import Column, String, Integer, DateTime, ForeignKey
+from extensions import db 
 from datetime import datetime
 from typing import Union
-from model.product import Product
-from extensions import db  # Importa o db globalmente definido
 
 class Comment(db.Model):
     __tablename__ = 'comment'
@@ -13,7 +11,13 @@ class Comment(db.Model):
     n_estrela = db.Column(db.Integer)
     date_inserted = db.Column(db.DateTime, default=datetime.now())
     
-    product = db.Column(db.Integer, db.ForeignKey('product.pk_product'), nullable=False)
+    # Correção: O campo que armazena a chave estrangeira (FK) é geralmente nomeado com '_id'
+    # Ele usa db.ForeignKey e aponta para 'tablename.column_name'
+    product_id = db.Column(db.Integer, db.ForeignKey('product.pk_product'), nullable=False)
+    
+    # Relação: Permite acessar o objeto Product a partir do Comment
+    product = db.relationship("Product", backref=db.backref('comments_list', lazy=True))
+
 
     def __init__(self, author: str, text: str, n_estrela: int = 0, 
              date_inserted: Union[datetime, None] = None):
@@ -23,4 +27,5 @@ class Comment(db.Model):
         if date_inserted:
             self.date_inserted = date_inserted
 
-
+    def __repr__(self):
+        return f'<Comment by {self.author}>'
--- model/product.py
+++ model/product.py
@@ -1,14 +1,10 @@
-# model/product.py
 from extensions import db  # Importa o db globalmente definido
 from datetime import datetime
 from sqlalchemy.orm import relationship
-from model.comment import Comment
-from extensions import db  # Importa o db globalmente definido
 
 class Product(db.Model):
     __tablename__ = 'product'
     
-    # AGORA USAMOS db.Column
     id = db.Column("pk_product", db.Integer, primary_key=True)
     name = db.Column(db.String(140), unique=True)
     barcode = db.Column(db.Integer, unique=True)
@@ -16,4 +12,3 @@ class Product(db.Model):
     # ... outros campos
 
     comments = db.relationship("Comment", backref="product") # Usando db.relationship
-    # ... outros métodos
