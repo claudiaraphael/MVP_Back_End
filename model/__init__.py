aa109c5035ca4e46378d76dc165351f6ded12db5
main <claudia.raphael73@gmail.com>
2025-12-05 20:23:58 -0300

    instance a session with the database

--- model/__init__.py
+++ model/__init__.py
@@ -8,9 +8,23 @@ from model.comment import Comment
 from model.product import Product
 from model.user import User
 
-# create the data base path
+# create the database path
 db_path = "database/"
 # if there is no directory...
 if not os.path.exists(db_path):
     # create directory
     os.makedirs(db_path)
+
+# url to access the database in local sqlite3
+db_url = 'sqlite:///%s/db.sqlite3' % db_path
+
+# create the engine to conect to the database
+engine = create_engine(db_url,echo=False)
+
+# instanciate a session with the database
+Session = sessionmaker(bind=engine)
+
+# create the database if it doesnt exist
+if not database_exists(engine.url):
+    create_database(engine.url)
+
